84680036946ae46db05feadbabfd2b319125e2b6
SivaSahitya <82566266+SivaSahitya@users.noreply.github.com>
2021-05-16 19:19:04 +0530

    Create 1.1Pythonscript.py

---
+++ 1.1Pythonscript.py
@@ -0,0 +1,17 @@
+>>> #1.1)Running instructions in Interactive interpreter and a Python Script
+>>> a=34
+>>> print(a)#prints the value of a
+34
+>>> type(a)#prints the data type
+<class 'int'>
+>>> b=12.6
+>>> type(b)
+<class 'float'>
+>>> c=26.700
+>>> print(c)
+26.7
+>>> print(type(c))
+<class 'float'>
+>>> d="Hello,welcome to cse-a"
+>>> type(d)
+<class 'str'>
