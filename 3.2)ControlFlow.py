6df83b63b3fd451d6349924264561847ab8b01ef
SivaSahitya <82566266+SivaSahitya@users.noreply.github.com>
2021-05-17 09:51:12 +0530

    Create 3.2)ControlFlow.py

---
+++ 3.2)ControlFlow.py
@@ -0,0 +1,12 @@
+#3.2)Implement a python script using for loop that loops over a sequence.
+print('Iterating list:')
+fruits=['Apple','Orange','Grapes','Banana']
+for f in fruits:
+    print(f)
+
+"""output:
+Iterating list:
+Apple
+Orange
+Grapes
+Banana
