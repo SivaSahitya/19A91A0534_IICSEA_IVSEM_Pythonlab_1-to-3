e833e0678e4f9f2724ac0719e28ddcdd43298536
SivaSahitya <82566266+SivaSahitya@users.noreply.github.com>
2021-05-17 09:31:07 +0530

    Create 3.1)ControlFlow.py

---
+++ 3.1)ControlFlow.py
@@ -0,0 +1,13 @@
+#3.1)Implement a python script for checking whether the citizen is eligible for vote or not.
+age=int(input('enter the age of person : '))
+if age>=18:
+        print('Eligible for Voting')
+else:
+    print('Not Eligible for Voting')
+
+"""output:
+        enter the age of person : 40
+        Eligible for Voting
+
+        enter the age of person : 11
+        Not Eligible for Voting
